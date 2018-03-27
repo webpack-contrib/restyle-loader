83a222b011d3f0b4a12f342804c0b8fa93a84f8d
shellscape <andrew@shellscape.org>
2018-03-27 11:08:51 -0400

    refactor: update code to Node v6

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Daniel Verejan
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Daniel Verejan
 */
-var loaderUtils = require("loader-utils"),
-	path = require("path");
-module.exports = function() {};
-module.exports.pitch = function(remainingRequest) {
-	this.cacheable && this.cacheable();
-	return [
-		"// restyle-loader: Search for a stylesheet <link> in the DOM and replace/update the href",
-		"var update = require(" + JSON.stringify("!" + path.join(__dirname, "replaceStyle.js")) + ")(",
-		"\trequire(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ")",
-		");",
-		"// Hot Module Replacement",
-		"if(module.hot) {",
-		"\tmodule.hot.accept(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ", function() {",
-		"\t\tupdate(require(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + "));",
-		"\t});",
-		"\tmodule.hot.dispose(function() { update(); });",
-		"}"
-	].join("\n");
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+module.exports = () => {};
+module.exports.pitch = function pitch(remainingRequest) {
+  const modulePath = path.join(__dirname, 'lib/replace-style.js');
+  const moduleLoader = JSON.stringify(`!${modulePath}`);
+  const request = loaderUtils.stringifyRequest(this, `!!${remainingRequest}`);
+
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  return `// restyle-loader: Search for a stylesheet <link> in the DOM and replace/update the href',
+var update = require(${moduleLoader})(
+  require(${request})
+);
+
+// Hot Module Replacement
+if(module.hot) {
+  module.hot.accept(${request}, function() {
+    update(require(${request}));
+  });
+  module.hot.dispose(update);
+}`;
 };
--- test/utils.js
+++ test/utils.js
@@ -1,83 +1,83 @@
-// Node v4 requires "use strict" to allow block scoped let & const
-"use strict";
 
-import MemoryFS from "memory-fs";
-import realFs from "fs";
-import webpack from "webpack";
-import path from "path";
-import jsdom from "jsdom";
+const realFs = require('fs');
+const path = require('path');
+const jsdom = require('jsdom/lib/old-api');
+const MemoryFS = require('memory-fs');
+const webpack = require('webpack');
 
-// import assert from "assert";
+let compiler;
+let jsdomHtml;
 
-var compiler;
-var jsdomHtml;
+module.exports = {
 
-export function setup(webpackConfig, _jsdomHtml) {
-	let fs = new MemoryFS();
-
-	jsdomHtml = _jsdomHtml;
-
-	// Makes webpack resolve restyle-loader to local folder instead of node_modules
-	Object.assign(webpackConfig, {
-	  resolveLoader: {
-	    alias: {
-	      "restyle-loader": path.resolve(__dirname, "../")
-	    }
-	  }
-	});
-
-	compiler = webpack(webpackConfig);
-
-	// Tell webpack to use our in-memory FS
-	compiler.inputFileSystem = fs;
-	compiler.outputFileSystem = fs;
-	compiler.resolvers.normal.fileSystem = fs;
-	compiler.resolvers.context.fileSystem = fs;
-
-	["readFileSync", "statSync"].forEach(fn => {
-	  // Preserve the reference to original function
-	  fs["mem" + fn] = fs[fn];
-
-	  compiler.inputFileSystem[fn] = function(_path) {
-	    // Fallback to real FS if file is not in the memoryFS
-	    if (fs.existsSync(_path)) {
-	      return fs["mem" + fn].apply(fs, arguments);
-	    } else {
-	      return realFs[fn].apply(realFs, arguments);
-	    }
-	  };
-	});
-
-	return fs;
-}
-
-export function runCompilerTest(hrefList, done) {
-  compiler.run((err, stats) => {
-  	debugger;
-    if (stats.compilation.errors.length) {
-      throw new Error(stats.compilation.errors);
-    }
-
-    const bundleJs = stats.compilation.assets["bundle.js"].source();
-
-    jsdom.env({
-      html: jsdomHtml,
-      src: [bundleJs],
-      done: (err, window) => {
-      	hrefList.forEach(src => {
-      		const regExp = new RegExp(src + "\\?\\w{8}$");
-      		const el = window.document.querySelector(`link[href*='${src}']`);
-
-      		expect(el).not.toBe(null);
-      		expect(el.href).toBeDefined();
-      		expect(regExp.test(el.href)).toBe(true);
-      	});
-
-        // free memory associated with the window
-        window.close();
-
-        done();
+  run(hrefList, done) {
+    compiler.run((err, stats) => {
+      if (stats.compilation.errors.length) {
+        throw new Error(stats.compilation.errors);
       }
+
+      const bundleJs = stats.compilation.assets['bundle.js'].source();
+
+      // expect(1 + 2).toBe(3);
+      // done();
+      jsdom.env({
+        html: jsdomHtml,
+        src: [bundleJs],
+        done: (e, window) => {
+          for (const src of hrefList) {
+            const regExp = new RegExp(`${src}\\?\\w{8}$`);
+            const el = window.document.querySelector(`link[href*='${src}']`);
+
+            expect(el).not.toBe(null);
+            expect(el.href).toBeDefined();
+            expect(regExp.test(el.href)).toBe(true);
+          }
+
+          // free memory associated with the window
+          window.close();
+
+          done();
+        },
+      });
     });
-  });
-}
+  },
+
+  setup(webpackConfig, html) {
+    const fs = new MemoryFS();
+
+    jsdomHtml = html;
+
+    // Makes webpack resolve restyle-loader to local folder instead of node_modules
+    Object.assign(webpackConfig, {
+      resolveLoader: {
+        alias: {
+          'restyle-loader': path.resolve(__dirname, '../'),
+        },
+      },
+    });
+
+    compiler = webpack(webpackConfig);
+
+    // Tell webpack to use our in-memory FS
+    compiler.inputFileSystem = fs;
+    compiler.outputFileSystem = fs;
+    compiler.resolvers.normal.fileSystem = fs;
+    compiler.resolvers.context.fileSystem = fs;
+
+    ['readFileSync', 'statSync'].forEach((fn) => {
+    // Preserve the reference to original function
+      fs[`mem${fn}`] = fs[fn];
+
+      compiler.inputFileSystem[fn] = (...args) => {
+        const [filePath] = args;
+        // Fallback to real FS if file is not in the memoryFS
+        if (fs.existsSync(filePath)) {
+          return fs[`mem${fn}`](...args);
+        }
+        return realFs[fn](...args);
+      };
+    });
+
+    return fs;
+  },
+};
